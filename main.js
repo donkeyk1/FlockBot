4622bd0576445ce73b9a9563ebc2c4028f14b9fe
donkeyk1 <kohli.daksh13@gmail.com>
2022-05-03 01:04:32 -0400

    Added help command

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ const prefix = '%';
 
 const{token} = require("./config.json");
 
+const { MessageEmbed } = require("discord.js");
+
+function calculateDifference (date){
+    var date2 = new Date();
+    var Difference_In_Time = date2.getTime() - date.getTime();
+    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);    
+    return Difference_In_Days;
+}
+
 client.once('ready', () =>{
     console.log('Flock Bot is online');
 });
@@ -19,11 +28,17 @@ client.on('message', message =>{
     if(command === 'ping'){
         message.channel.send('pong!');
     } else if (command == 'raf'){
-        var date1 = new Date("09/05/1995");
-        var date2 = new Date();
-    var Difference_In_Time = date2.getTime() - date1.getTime();
-    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-    message.channel.send(`Days since <@163440449925808129> has been funny: ` + (Difference_In_Days|0));
+        var days = calculateDifference(new Date("09/05/1995"));
+        message.channel.send(`Days since <@163440449925808129> has been funny: ` + (days|0));
+    } else if (command == 'jiggs'){
+        var days = calculateDifference(new Date("06/08/2002"));
+        message.channel.send(`Days since <@360488559309750273> has touched grass: ` + (days|0));
+    } else if (command == 'donkey'){
+        var days = calculateDifference(new Date("10/19/2002"));
+        message.channel.send(`Days since <@225032479734628353> has had a personality: ` + (days|0));
+    } else if (command == 'help'){
+        message.channel.send(`%raf: Makes fun of Raf\n%jiggs: Makes fun of Jiggs\n%donkey: Makes fun of Donkey\n%help: Displays commands\n`);
+        
     }
 });
 
